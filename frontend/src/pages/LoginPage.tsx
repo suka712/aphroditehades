f3f7b1120422193a0404658287f7581465b7cfed
khiem <khiemnguyen.hye@gmail.com>
2025-12-31 03:29:57 +0700

    Auth forms: Adjust responsive top padding.

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -2,7 +2,7 @@ import { LoginForm } from "@/components/login-form"
 
 const LoginPage = () => {
   return (
-    <div className="flex justify-center items-center pt-35">
+    <div className="flex justify-center items-center pt-10 md:pt-25">
       <LoginForm />
     </div>
   )
